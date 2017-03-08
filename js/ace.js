2bc06ac5f6721fdfb826a31708ff8761fcf984e0
brucewayne <brucewaynesonofgotham@gmail.com>
2017-03-09 00:51:30 +0300

    second push

--- js/ace.js
+++ js/ace.js
@@ -1,10 +1,14 @@
-var editor = ace.edit("editor");
-editor.setTheme("ace/theme/chaos");
-editor.getSession().setMode("ace/mode/html");
-var iframe = document.querySelector('iframe');
-var doc = iframe.contentWindow.document;
-editor.getSession().on('change', function() {
-    // Вывод содержимого редактора в iframe
-    doc.body.innerHTML = editor.getValue();
-});
-editor.setValue("<!DOCTYPE html>\n<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t<p>Привет, рукожоп! Давай научимся верстать вместе!</p>\n\t</body>\n</html>");
+function acejs() {
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/chaos");
+    editor.getSession().setMode("ace/mode/html");
+    var iframe = document.querySelector('iframe');
+    var doc = iframe.contentWindow.document;
+    editor.getSession().on('change', function() {
+        // Вывод содержимого редактора в iframe
+        doc.body.innerHTML = editor.getValue();
+    });
+    editor.setValue("<!DOCTYPE html>\n<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t<p>Привет, рукожоп! Давай научимся верстать вместе!</p>\n\t</body>\n</html>");
+}
+
+acejs();
--- js/ace1.js
+++ js/ace1.js
@@ -1,9 +1,16 @@
-var editor = ace.edit("editor1");
-editor.setTheme("ace/theme/monokai");
-editor.getSession().setMode("ace/mode/html");
-var iframe = document.querySelector('iframe');
-var doc = iframe.contentWindow.document;
-editor.getSession().on('change', function() {
-    // Вывод содержимого редактора в iframe
-    doc.body.innerHTML = editor.getValue();
-});
+function acejs1() {
+    var editor = ace.edit("editor1");
+    editor.setTheme("ace/theme/monokai");
+    editor.getSession().setMode("ace/mode/css");
+    var iframe = document.querySelector('iframe');
+    var doc = iframe.contentWindow.document;
+    var style = doc.createElement('style');
+    doc.head.appendChild(style);
+    editor.getSession().on('change', function() {
+        // Вывод содержимого редактора в iframe
+        doc.head.lastChild.innerHTML = editor.getValue();
+    });
+
+}
+
+acejs1();
