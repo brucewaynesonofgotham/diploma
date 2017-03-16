034a51cfb30204d6401317afbd1c2582e5d2346b
brucewayne <brucewaynesonofgotham@gmail.com>
2017-03-16 16:17:48 +0300

    fixed iframe

--- js/ace.js
+++ js/ace.js
@@ -8,7 +8,9 @@ function acejs() {
         // Вывод содержимого редактора в iframe
         doc.body.innerHTML = editor.getValue();
     });
-    editor.setValue("<!DOCTYPE html>\n<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t<p>Привет, рукожоп! Давай научимся верстать вместе!</p>\n\t</body>\n</html>");
+    editor.setValue("<!DOCTYPE html>\n<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t<p>Привет, рукожоп! " +
+        "Давай научимся верстать вместе!</p>\n\t\t<img src='http://orig03.deviantart.net/11e8/f/2007/035/6/b/batman___black_and_white_by_nafasmotor.jpg'> " +
+        "\n\t</body>\n</html>");
 }
 
 acejs();
