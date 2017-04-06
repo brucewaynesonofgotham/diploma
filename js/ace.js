79e2d01deb7355b73736144f7c5616e38e4267a5
brucewayne <brucewaynesonofgotham@gmail.com>
2017-04-06 08:00:49 +0300

    added theory and tasks

--- js/ace.js
+++ js/ace.js
@@ -10,34 +10,20 @@ function acejs() {
     var docPc = pcIframe.contentWindow.document;
     var iframe = document.querySelector('iframe');
     var doc = iframe.contentWindow.document;
+    var currentContent ='';
     editor.getSession().on('change', function() {
         // Вывод содержимого редактора в iframe
         doc.body.innerHTML = editor.getValue();
         docIphone.body.innerHTML = editor.getValue();
         docIpad.body.innerHTML = editor.getValue();
         docPc.body.innerHTML = editor.getValue();
+        currentContent = editor.getValue();
     });
-    editor.setValue('<!DOCTYPE html><html>\n\t<head>\n\t</head>\n\t<body>\n\t\t<picture>\n\t\t\t<source srcset="/images/400.jpg" media="(max-width: 500px)">' +
-        '\n\t\t\t<source srcset="/images/800.jpg" media="(max-width: 900px)">\n\t\t\t<source srcset="/images/1200.jpg">' +
-        '\n\t\t\t<img srcset="/images/400.jpg" alt="My default image">\n\t\t</picture>\n\t</body>\n</html>');
+    editor.setValue('<!DOCTYPE html>\n<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t<picture>\n\t\t\t<source srcset="/images/400.jpg">' +
+        '\n\t\t\t<source srcset="/images/800.jpg">\n\t\t\t<source srcset="/images/1200.jpg">' +
+        '\n\t\t\t<source srcset="/images/400.jpg">\n\t\t\t<img srcset="/images/400.jpg" alt="My default image">\n\t\t</picture>\n\t</body>\n</html>' +
+        '\n<!--Не успел запилить анимацию на индикаторах-->');
 }
+
 acejs();
 
-/*
-<img
-src="https://image.ibb.co/gsqeiv/400.jpg"
-srcset=" 400w, https://image.ibb.co/g4dRVa/800.jpg 800w, https://image.ibb.co/c5DtAa/1200.jpg 1200w"
-sizes="(min-width: 700px) 75vw, 100vw"
-    >*/
-/*
-<!DOCTYPE html>
-<html>
-<head>
-</head>
-<body>
-<p>Привет, рукожоп! Давай научимся верстать вместе!</p>
-<img src="https://image.ibb.co/gsqeiv/400.jpg"
-srcset="400w, https://image.ibb.co/g4dRVa/800.jpg 800w, https://image.ibb.co/c5DtAa/1200.jpg 1200w"
-sizes="(min-width: 400px) 100vw, (min-width: 800px) 100vw, (min-width: 1200px) 100vw, 100vw">
-    </body>
-    </html>*/
--- js/ace1.js
+++ js/ace1.js
@@ -12,5 +12,13 @@ function acejs1() {
     });
 
 }
-
+$( "#go" ).click(function() {
+    $( "#block" ).animate({
+        width: "70%",
+        opacity: 0.4,
+        marginLeft: "0.6in",
+        fontSize: "3em",
+        borderWidth: "10px"
+    }, 1500 );
+});
 acejs1();
