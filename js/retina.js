72db040b56440592cad653ceb882a17cdeb300ca
brucewayne <brucewaynesonofgotham@gmail.com>
2017-04-20 07:32:09 +0300

    big update: width settings are completed

--- js/retina.js
+++ js/retina.js
@@ -1,6 +1,53 @@
-function retina() {
+var onOff = document.getElementById('on-off');
+onOff.addEventListener("click", function() {
     var iframe = document.querySelector('iframe');
     var doc = iframe.contentWindow.document;
+
+    var placeForAce = document.getElementById('place-for-ace');
+    var browser = document.getElementById('browser');
+    
+    var editorH = document.getElementById('editor');
+    
+    var radio4 = document.getElementById('optionsRadios4');
+    var radio5 = document.getElementById('optionsRadios5');
+    var radio6 = document.getElementById('optionsRadios6');
+
+    if (radio4.checked && (onOff.className === ('btn btn-info btn-md focus' || 'btn btn-info btn-md'))){
+        placeForAce.className = '';
+        placeForAce.className = 'col-md-6';
+        editorH.style.height = '618px';
+        iframe.style.height = '305px';
+        browser.style.margin = '0';
+        browser.className = '';
+        browser.className = 'col-md-4 browser';
+    }else if (radio5.checked && (onOff.className === ('btn btn-info btn-md focus' || 'btn btn-info btn-md'))){
+        placeForAce.className = '';
+        placeForAce.className = 'col-md-4';
+        editorH.style.height = '618px';
+        iframe.style.height = '345px';
+        browser.style.margin = '0';
+        browser.className = '';
+        browser.className = 'col-md-8 browser';
+    }else if (radio6.checked && (onOff.className === ('btn btn-info btn-md focus' || 'btn btn-info btn-md'))){
+        placeForAce.className = '';
+        placeForAce.className = 'col-md-12';
+        editorH.style.height = '280px';
+        iframe.style.height = '345px';
+        browser.style.marginTop = '20px';
+        browser.style.marginLeft = '12px';
+        browser.style.maxWidth = '1144px';
+        browser.className = '';
+        browser.className = 'col-md-12 browser';
+    }else{
+        placeForAce.className = '';
+        placeForAce.className = 'col-md-4';
+        editorH.style.height = '618px';
+        iframe.style.height = '345px';
+        browser.style.margin = '0';
+        browser.className = '';
+        browser.className = 'col-md-8 browser';
+    }
+
     (function (window, location) {
 
         function upperCaseFirst(str) {
@@ -26,11 +73,6 @@ function retina() {
         };
 
         var retinaStyles = retinaChoice;
-
-        /*var retinaStyles ={
-            "transform": "scale(2)",
-            "transformOrigin": "0 0"
-        };*/
 
         var nonRetinaStyles = {
             "transform": "",
@@ -75,32 +117,78 @@ function retina() {
         }, 10);
 
     })(window, location);
-}
+});
 var retinaChoice ={
-    "transform": "scale(2)",
+    "transform": "scale(1)",
     "transformOrigin": "0 0"
 };
 
-function changeScale() {
-    if (radio1.checked) {
-        retinaChoice = {
-            "transform": "scale(1)",
-            "transformOrigin": "0 0"
-        }
-    }else if (radio2.checked){
-        retinaChoice = {
-            "transform": "scale(2)",
-            "transformOrigin": "0 0"
-        }
-    }else if (radio3.checked) {
-        retinaChoice = {
-            "transform": "scale(3)",
-            "transformOrigin": "0 0"
-        }
+document.getElementById('optionsRadios1').addEventListener("click", function () {
+    retinaChoice = {
+        "transform": "scale(1)",
+        "transformOrigin": "0 0"
     }
-}
+});
+
+document.getElementById('optionsRadios2').addEventListener("click", function () {
+    retinaChoice = {
+        "transform": "scale(2)",
+        "transformOrigin": "0 0"
+    }
+});
+
+document.getElementById('optionsRadios3').addEventListener("click", function () {
+    retinaChoice = {
+        "transform": "scale(3)",
+        "transformOrigin": "0 0"
+    }
+});
 
 
-var radio1 = document.getElementById('optionsRadios1');
-var radio2 = document.getElementById('optionsRadios2');
-var radio3 = document.getElementById('optionsRadios3');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+if (radio4.checked) {
+
+ }else if (radio5.checked){
+ $('toggle-view').on('click',function () {
+ $('place-for-ace').toggleClass("col-md-6 col-md-12");
+ $('browser').toggleClass("col-md-6 col-md-12");
+ });
+ }else if (radio6.checked) {
+ $('toggle-view').on('click',function () {
+ $('place-for-ace').toggleClass("col-md-6 col-md-12");
+ $('browser').toggleClass("col-md-6 col-md-12");
+ });
+ }
+
+$('toggle-view').click(function () {
+    $('place-for-ace').swithClass("col-md-6 col-md-12");
+    $('browser').toggleClass("col-md-6 col-md-12");
+});*/
