c5b8f9756fb69c52d833c8d3d469a5cf6db25268
brucewayne <brucewaynesonofgotham@gmail.com>
2017-04-20 08:08:15 +0300

    bad animation on indicators

--- js/ace.js
+++ js/ace.js
@@ -4,15 +4,28 @@ function acejs() {
     var iframe = document.querySelector('iframe');
     var doc = iframe.contentWindow.document;
     var currentContent ='';
+    var firstTask = document.getElementById('first-task');
+    var secondTask = document.getElementById('second-task');
+    var nextTask = document.getElementById('next-task');
     editor.getSession().on('change', function() {
         // Вывод содержимого редактора в iframe
         doc.body.innerHTML = editor.getValue();
         currentContent = editor.getValue();
+        if (currentContent == '<!DOCTYPE html>\n<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t<picture>\n\t\t\t<source srcset="/images/400.jpg" media="(max-width: 500px)">' +
+            '\n\t\t\t<source srcset="/images/800.jpg">\n\t\t\t<source srcset="/images/1200.jpg">' +
+            '\n\t\t\t<source srcset="/images/400.jpg">\n\t\t\t<img srcset="/images/400.jpg" alt="My default image">\n\t\t</picture>\n\t</body>\n</html>'){
+            firstTask.style.backgroundColor = 'yellow';
+        }
+        if (currentContent == '<!DOCTYPE html>\n<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t<picture>\n\t\t\t<source srcset="/images/400.jpg" media="(max-width: 500px)">' +
+            '\n\t\t\t<source srcset="/images/800.jpg" media="(max-width: 900px)">\n\t\t\t<source srcset="/images/1200.jpg" media="(max-width: 1600px)">' +
+            '\n\t\t\t<source srcset="/images/400.jpg">\n\t\t\t<img srcset="/images/400.jpg" alt="My default image">\n\t\t</picture>\n\t</body>\n</html>') {
+            secondTask.style.backgroundColor = 'yellow';
+            nextTask.removeAttribute('disabled');
+        }
     });
     editor.setValue('<!DOCTYPE html>\n<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t<picture>\n\t\t\t<source srcset="/images/400.jpg">' +
         '\n\t\t\t<source srcset="/images/800.jpg">\n\t\t\t<source srcset="/images/1200.jpg">' +
-        '\n\t\t\t<source srcset="/images/400.jpg">\n\t\t\t<img srcset="/images/400.jpg" alt="My default image">\n\t\t</picture>\n\t</body>\n</html>' +
-        '\n<!--Не успел запилить анимацию на индикаторах-->');
+        '\n\t\t\t<source srcset="/images/400.jpg">\n\t\t\t<img srcset="/images/400.jpg" alt="My default image">\n\t\t</picture>\n\t</body>\n</html>');
 }
 
 acejs();
